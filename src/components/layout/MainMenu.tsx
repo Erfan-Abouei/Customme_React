6272f1b4332dbb264e3b14849bff6ca0762cf381
Erfan-Abouei <erfangamer138787@gmail.com>
2025-07-10 15:31:04 +0330

    add fade element & optimize search bar with useMemo and useCallback hook

--- src/components/layout/MainMenu.tsx
+++ src/components/layout/MainMenu.tsx
@@ -4,12 +4,12 @@ import { HiOutlinePaintBrush } from "react-icons/hi2";
 const MainMenu = () => {
     return (
         <div className="container">
-            <div className="mt-8 max-md:hidden flex items-center gap-x-8">
+            <div className="relative mt-8 max-md:hidden flex items-center gap-x-8">
                 <PrimaryButton customClass="cursor-pointer text-white font-iran-medium text-sm bg-primary hover:bg-primary/80 rounded-lg max-w-46 w-full h-12">
                     <HiOutlinePaintBrush className="size-6" />
                     <span> خودت طراحیش کن !</span>
                 </PrimaryButton>
-                <div className="relative whitespace-nowrap overflow-auto page__scroll--hidden flex items-center gap-x-10">
+                <div className="whitespace-nowrap overflow-auto page__scroll--hidden flex items-center gap-x-10">
                     {/* Span tag ( replace Link Component ) */}
                     <span className="font-iran-regular text-base text-gray-900 hover:text-gray-950 cursor-pointer transition-all">دسته بندی محصولات</span>
                     <span className="font-iran-regular text-base text-gray-900 hover:text-gray-950 cursor-pointer transition-all">دسته بندی محصولات</span>
