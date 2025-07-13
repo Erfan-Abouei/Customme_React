7ef10f5084831d62d4a941de3076ee99538fc7c4
Erfan-Abouei <erfangamer138787@gmail.com>
2025-07-14 01:07:52 +0330

    install react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import LoadingScreen from './components/layout/LoadingScreen.tsx'
+import { QueryClientProvider } from '@tanstack/react-query'
+import queryClient from './config/queryClient.ts'
 import App from './App.tsx'
 import store from './store/index.tsx'
 import { Provider } from 'react-redux'
@@ -12,9 +14,11 @@ import 'nprogress/nprogress.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Suspense fallback={<LoadingScreen />} >
-      <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
           <App />
-      </Provider>
+        </Provider>
+      </QueryClientProvider>
       <Toaster
         position="bottom-left"
       />
