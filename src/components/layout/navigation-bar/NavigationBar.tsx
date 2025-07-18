72ce65c77c6082f23f4b7d20dd95121f76c861a1
Erfan-Abouei <erfangamer138787@gmail.com>
2025-07-19 01:21:19 +0330

    refactor: navigation style

--- src/components/layout/navigation-bar/NavigationBar.tsx
+++ src/components/layout/navigation-bar/NavigationBar.tsx
@@ -11,11 +11,9 @@ import { HiMiniShoppingCart as CartActive } from "react-icons/hi2";
 import { HiMiniUser as UserActive } from "react-icons/hi2";
 import { HiOutlineUser as UserNotActive } from "react-icons/hi2";
 
-
-
 const NavigationBar = () => {
     return (
-        <div className="md:hidden fixed w-full h-20 px-10 bg-primary flex items-center justify-between gap-x-2 bottom-0 left-0 right-0">
+        <div className="md:hidden fixed w-9/10 mx-auto h-20 px-6 bg-primary flex items-center justify-between gap-x-2 bottom-4 left-0 right-0 rounded-2xl">
             <NavigationBarLink activeIcon={<HomeActive className="size-full" />} icon={<HomeNotActive className="size-full" />} path="/" title="خانه" />
             <NavigationBarLink activeIcon={<BrushActive className="size-full" />} icon={<BrushNotActive className="size-full" />} path="/custom-design" title="طراحی سفارشی" />
             <NavigationBarLink activeIcon={<CartActive className="size-full" />} icon={<CartNotActive className="size-full" />} path="/cart" title="سبد خرید" />
