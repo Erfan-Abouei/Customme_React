8d69b80b6d61ca4ccf0096c63506f867632b90d1
Erfan-Abouei <erfangamer138787@gmail.com>
2025-07-22 10:19:32 +0330

    add: reusable component ( special item box )

--- src/pages/home/components/special-sell-section/OfferComonent.tsx
+++ src/pages/home/components/special-sell-section/OfferComonent.tsx
@@ -1,7 +1,9 @@
-const OfferComponent = () => {
+import type { OfferComponentProp } from "@/types/components-props.types"
+
+const OfferComponent = ({ discountNumber }: OfferComponentProp) => {
     return (
         <div className="max-w-14.25 max-lg:max-w-10 w-full max-md:max-w-6 absolute top-0 left-4 flex flex-col items-center gap-y-1 offer__card--discount-number h-25 max-lg:h-15 max-md:h-11 text-white font-dana py-2">
-            <span className="text-base max-md:text-custom">20%</span>
+            <span className="text-base max-md:text-custom">{discountNumber}%</span>
             <span className="max-lg:hidden">تخفیف</span>
         </div>
     )
--- src/pages/home/components/special-sell-section/SpecialSellContainer.tsx
+++ src/pages/home/components/special-sell-section/SpecialSellContainer.tsx
@@ -1,47 +1,17 @@
-import OfferComponent from "./OfferComonent"
+import SpecialItemBox from "./SpecialItemBox"
 
 const SpecialSellContainer = () => {
     return (
         <div className="mt-6 max-md:mt-3 grid grid-cols-12 max-md:grid-cols-4 gap-x-6 max-md:gap-x-4 items-center">
-            <div className="relative col-span-6 max-md:col-span-2 h-144 max-lg:h-100 max-md:h-33.5 max-md:rounded-lg rounded-2xl special-sell__item overflow-hidden">
-                <OfferComponent />
-                {/* Item Title */}
-                <span className="absolute bottom-8 right-8 max-md:bottom-0 max-md:left-4 text-2xl line-clamp-1 max-md:text-sm text-primary-tint-300 font-dana-semi-bold max-md:font-dana">لوازم خانه </span>  
-                {/* Item Image */}
-                <img src="/images/Special-Item-Image.png" alt="Test" className="size-full object-cover absolute inset-0 -z-1" />
-            </div>
+            <SpecialItemBox className="relative col-span-6 max-md:col-span-2 h-144 max-lg:h-100 max-md:h-33.5 max-md:rounded-lg rounded-2xl special-sell__item overflow-hidden" boxImage="/images/Special-Item-Image.png" boxTitle="تست" />
             <div className="col-span-6 max-md:col-span-2 h-full max-md:rounded-lg flex items-center justify-center flex-col gap-y-4 max-md:gap-y-1.5">
                 <div className="size-full flex items-center gap-x-6 max-md:gap-x-4">
-                    <div className="size-full rounded-2xl max-md:rounded-lg relative special-sell__item overflow-hidden">
-                        <OfferComponent />
-                        {/* Item Title */}
-                        <span className="absolute bottom-8 right-8 max-md:bottom-0 max-md:left-4 text-2xl line-clamp-1 max-md:text-sm text-primary-tint-300 font-dana-semi-bold max-md:font-dana">لوازم مدرسه</span>
-                        {/* Item Image */}
-                        <img src="/images/Special-Item-Image.png" alt="Test" className="size-full object-cover absolute inset-0 -z-1" />
-                    </div>
-                    <div className="size-full rounded-2xl max-md:rounded-lg relative special-sell__item overflow-hidden">
-                        <OfferComponent />
-                        {/* Item Title */}
-                        <span className="absolute bottom-8 right-8 max-md:bottom-0 max-md:left-4 text-2xl line-clamp-1 max-md:text-sm text-primary-tint-300 font-dana-semi-bold max-md:font-dana">پوشاک</span>
-                        {/* Item Image */}
-                        <img src="/images/Special-Item-Image.png" alt="Test" className="size-full object-cover absolute inset-0 -z-1" />
-                    </div>
+                    <SpecialItemBox className="size-full rounded-2xl max-md:rounded-lg relative special-sell__item overflow-hidden" boxImage="/images/Special-Item-Image.png" boxTitle="تست" />
+                    <SpecialItemBox className="size-full rounded-2xl max-md:rounded-lg relative special-sell__item overflow-hidden" boxImage="/images/Special-Item-Image.png" boxTitle="تست" />
                 </div>
                 <div className="size-full flex items-center gap-x-6 max-md:gap-x-4">
-                    <div className="size-full rounded-2xl max-md:rounded-lg relative special-sell__item overflow-hidden">
-                        <OfferComponent />
-                        {/* Item Title */}
-                        <span className="absolute bottom-8 right-8 max-md:bottom-0 max-md:left-4 text-2xl line-clamp-1 max-md:text-sm text-primary-tint-300 font-dana-semi-bold max-md:font-dana">دیجیتال</span>
-                        {/* Item Image */}
-                        <img src="/images/Special-Item-Image.png" alt="Test" className="size-full object-cover absolute inset-0 -z-1" />
-                    </div>
-                    <div className="size-full rounded-2xl max-md:rounded-lg relative special-sell__item overflow-hidden">
-                        <OfferComponent />
-                        {/* Item Title */}
-                        <span className="absolute bottom-8 right-8 max-md:bottom-0 max-md:left-4 text-2xl line-clamp-1 max-md:text-sm text-primary-tint-300 font-dana-semi-bold max-md:font-dana">استیکر</span>
-                        {/* Item Image */}
-                        <img src="/images/Special-Item-Image.png" alt="Test" className="size-full object-cover absolute inset-0 -z-1" />
-                    </div>
+                    <SpecialItemBox className="size-full rounded-2xl max-md:rounded-lg relative special-sell__item overflow-hidden" boxImage="/images/Special-Item-Image.png" boxTitle="تست" />
+                    <SpecialItemBox className="size-full rounded-2xl max-md:rounded-lg relative special-sell__item overflow-hidden" boxImage="/images/Special-Item-Image.png" boxTitle="تست" />
                 </div>
             </div>
         </div>
